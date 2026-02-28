12a45dc8761e65abd2a83f2391eeca71fa8fe25e
haning47 <155292432+haning47@users.noreply.github.com>
2026-02-28 22:43:26 +0800

    Update TagList.js

--- themes/next/components/TagList.js
+++ themes/next/components/TagList.js
@@ -1,7 +1,7 @@
 import TagItem from './TagItem'
 
 /**
- * 横向的标签列表
+ * 横向的標籤列表
  * @param tags
  * @param currentTag
  * @returns {JSX.Element}
@@ -12,7 +12,7 @@ const TagList = ({ tagOptions, currentTag }) => {
     return <></>
   }
   return <ul className='flex py-1 space-x-3'>
-    <li className='w-20 py-2 dark:text-gray-200 whitespace-nowrap'>标签:</li>
+    <li className='w-20 py-2 dark:text-gray-200 whitespace-nowrap'>標籤:</li>
     {tagOptions.map(tag => {
       const selected = tag.name === currentTag
       return <TagItem key={tag.name} tag={tag} selected={selected}/>
